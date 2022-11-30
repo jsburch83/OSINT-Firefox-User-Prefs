f6c065963380d73c6fcc6df50bb137d8a2d358b4
jacob b <jsburch83@gmail.com>
2022-11-30 17:37:02 -0600

    Update user.js
    added more firefox settings for OSINT

--- user.js
+++ user.js
@@ -10,3 +10,18 @@ pref("extensions.pocket.enabled", false);
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 //Disable Home Content - shortcuts
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+//Disable Home Content - Web Search
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+//Delete cookies and site data when Firefox is closed
+user_pref("network.cookie.lifetimePolicy", 2);
+//Ask to save logins and passwords for websites - Disable
+user_pref("signon.rememberSignons", false);
+//Show alerts about passwords for breached websites - Disable
+user_pref("signon.management.page.breach-alerts.enabled", false);
+//Firefox will use custom settings for history
+user_pref("privacy.history.custom", true);
+//under History - Remember browsing and download history
+user_pref("places.history.enabled", false);
+//under History - Remember search and form history
+user_pref("browser.formfill.enable", false);
+
