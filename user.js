5a7fd5b2949a4247431c0796733fe8fabac9d8cb
jacob b <jsburch83@gmail.com>
2022-11-30 14:34:23 -0600

    Create user.js
    first commit

---
+++ user.js
@@ -0,0 +1,6 @@
+//Homepage set to Blank Page
+user_pref("browser.startup.page", 0);
+//Recommend Features as you browse - Disable 
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features",false);
+//Recommend extensions as you browse - Disable
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
