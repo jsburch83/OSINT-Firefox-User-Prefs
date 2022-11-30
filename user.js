5fb763d663ebf817e4c0d703adfa7a346829adde
jacob b <jsburch83@gmail.com>
2022-11-30 16:47:18 -0600

    Update user.js

--- user.js
+++ user.js
@@ -4,3 +4,9 @@ user_pref("browser.startup.page", 0);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features",false);
 //Recommend extensions as you browse - Disable
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+//Home Content - Disable Pocket Add-On
+pref("extensions.pocket.enabled", false);
+//Disable Home Content - Recommended By Pocket
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+//Disable Home Content - shortcuts
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
