42ebae44b82f5f17da1393bba152353607cabcca
jacob b <jsburch83@gmail.com>
2022-11-30 22:33:02 -0600

    Update user.js
    add more user prefs

--- user.js
+++ user.js
@@ -1,3 +1,6 @@
+//Add this user.js file to your firefox profile directory
+
+
 //Homepage set to Blank Page
 user_pref("browser.startup.page", 0);
 //Recommend Features as you browse - Disable 
@@ -41,3 +44,34 @@ user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
 //HTTPS-only mode
 user_pref("dom.security.https_only_mode", true);
+//Disables firefox from sharing your location
+user_pref("geo.enabled", false);
+//blocks sending battery level information
+user_pref("dom.battery.enabled", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+//disable telemetry
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("toolkit.telemetry.server","");
+user_pref("toolkit.telemetry.unified", false);
+user_pref("media.autoplay.default", 5);
+//disable embedded notifications
+user_pref("dom.webnotifications.enabled", false); 
+//Enable Firefox's anti-fingerprinting mode (may block the installation of containers)
+user_pref("privacy.resistFingerprinting", true);
+//disable fingerprinting
+user_pref("webgl.disabled", true);
+//referring website - disable
+user_pref("network.http.sendRefererHeader", 0);
+//disables any embedded Firefox accounts
+user_pref("identity.fxaccounts.enabled", false);
+//disable crash reporting
+user_pref("browser.tabs.crashReporting.sendReport", false);
+//prevents some malicious PDF actions
+user_pref("pdfjs.enableScripting", false);
+//disable prefetch
+user_pref("network.dns.disablePrefetch", true);
+//disables prefectching also
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("network.prefetch-next", false);
